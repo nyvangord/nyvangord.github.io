7ad9cfa992c3c9e2559811e6b616ce511b854faf
nyvangord <nyvangord@gmail.com>
2016-07-12 18:49:27 -0400

    Updated final project.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 $(function() {
-	$('.my-slider').unslider();
-	var slider = $('.my-demo-slider').unslider();
 
-	$(function() { $('.banner').unslider() })
+	$('.slideshow').slick({
+		autoplay: true,
+		});
 
+	$('.variable-width').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 100,
+	  slidesToShow: 1,
+	  centerMode: true,
+	  variableWidth: true,   
+	});
 
-
+	$('.one-time').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 100,
+	  slidesToShow: 1,
+	  adaptiveHeight: true
+	  // height: 400px
+	});
 
 });
+
+	
